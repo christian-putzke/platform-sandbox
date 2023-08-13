4e5e49afbae3a5ddb4f21ef3f22cf8c8af26154a
Christian Putzke <mail@christian-putzke.de>
2023-08-13 12:07:18 +0200

    Improved moving and adding jumping to character controller based movement

--- Assets/Source/gameplay/player/Player3.cs
+++ Assets/Source/gameplay/player/Player3.cs
@@ -1,5 +1,6 @@
 using plasa.input;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace plasa.gameplay.player
 {
@@ -8,33 +9,145 @@ namespace plasa.gameplay.player
 	{
 		[SerializeField]
 		private CharacterController _characterController;
-        
+
+		[Header("Horizontal Speed")]
 		[SerializeField]
-		private float _movementSpeed;
+		private float _horizontalSpeed;
+
+		[Range(0,1)]
+		[SerializeField]
+		private float _horizontalSpeedReductionWhileJumping;
+
+		[SerializeField]
+		private float _horizontalSpeedReductionWhileGrounded;
+
+		[Header("Vertical Speed")]
+		[SerializeField]
+		private float _jumpForce = 1f;
+
+		[Header("Gravity")]
+		[SerializeField]
+		private float _minFallSpeed = 8f;
+
+		[SerializeField]
+		private float _maxFallSpeed = 15f;
+
+		[SerializeField]
+		private float _timeMaxFallSpeed = 0.5f;
+
+		[Header("Ground Check")]
+		[SerializeField]
+		private LayerMask _groundCheckLayer;
+
+		[SerializeField]
+		private Transform _groundCheckPosition;
+
+		[SerializeField]
+		private Vector3 _groundCheckSize;
 
 
-        private InputActions _inputActions;
+		private InputActions _inputActions;
+		private Vector3 _speed = Vector3.one;
+		private Vector3 _speedWhenJumpStarted;
 
-        private void Awake() {
+		private void Awake()
+		{
 			_characterController = GetComponent<CharacterController>();
 
 			_inputActions = new InputActions();
 			_inputActions.Enable(); // be careful, this enables ALL control schemes (keyboard AND gamepad)
-			// _inputActions.Player.Jump.performed += Jump;
 		}
 
-		private void FixedUpdate()
+		public bool IsGrounded()
 		{
-			if (_inputActions.Player.Move.IsInProgress()) {
-				var direction = _inputActions.Player.Move.ReadValue<Vector2>();
-				Move(direction);
-			}
+			var result = Physics.OverlapBox(_groundCheckPosition.position, _groundCheckSize / 2, Quaternion.identity, _groundCheckLayer);
+			return result.Length > 0;
 		}
 
-        private void Move(Vector3 direction)
-        {
-			var movement = new Vector3(direction.x * _movementSpeed, 0f, 0f);
+		private void Update()
+		{
+			var isGrounded = IsGrounded();
+
+			debug.ui.WriteText($"_speed: {_speed}", true);
+			debug.ui.WriteText($"_speedWhenJumpStarted: {_speedWhenJumpStarted}");
+			debug.ui.WriteText($"isGrounded: {isGrounded}");
+
+			if (isGrounded && _inputActions.Player.Jump.WasPerformedThisFrame())
+			{
+				_speedWhenJumpStarted = _speed;
+				_speed.y = 5f;
+			}
+			else if (!isGrounded)
+			{
+				_speed.y -= 0.01f;
+			}
+			else if (_speed.y <= 0.01f && isGrounded)
+			{
+				_speed.y = 0f;
+			}
+
+			if (isGrounded)
+			{
+				_speed = HorizontalMovementWhileGrounded(_speed);
+			}
+			else
+			{
+				_speed = HorizontalMovementWhileJumping(_speed);
+			}
+
+			Move(_speed);
+		}
+
+		private Vector3 HorizontalMovementWhileGrounded(Vector3 speed)
+		{
+			if (_inputActions.Player.Move.IsInProgress())
+			{
+				var direction = _inputActions.Player.Move.ReadValue<Vector2>();
+				speed.x = direction.x * _horizontalSpeed;
+			}
+			else
+			{
+				// TODO: Improve by adding lerp
+				if (speed.x > 0f)
+				{
+					speed.x = Mathf.Max(_speed.x - _horizontalSpeedReductionWhileGrounded, 0f);
+				}
+				else
+				{
+					speed.x = Mathf.Min(_speed.x + _horizontalSpeedReductionWhileGrounded, 0f);
+				}
+			}
+
+			return speed;
+		}
+
+		private Vector3 HorizontalMovementWhileJumping(Vector3 speed)
+		{
+			if (_inputActions.Player.Move.IsInProgress())
+			{
+				var direction = _inputActions.Player.Move.ReadValue<Vector2>();
+
+				if (direction.x > 0f && speed.x < 0f || direction.x < 0f && speed.x > 0f)
+				{
+					speed.x = _speedWhenJumpStarted.x * _horizontalSpeedReductionWhileJumping;
+				}
+			}
+
+			return speed;
+		}
+
+		private void Move(Vector3 speed)
+		{
+			var movement = new Vector3(speed.x * Time.deltaTime, speed.y * Time.deltaTime, 0f);
+			debug.ui.WriteText($"movement: {movement}");
 			_characterController.Move(movement);
-        }
-    }
+		}
+
+		private void OnDrawGizmos()
+		{
+			// Show ground check overlap box
+			Gizmos.color = Color.red;
+			Gizmos.DrawCube(_groundCheckPosition.position, _groundCheckSize);
+		}
+	}
 }
